3aafac1b9cd32692c2a00fdb98e647a869e202c0
YrecPodlecc <000333555666@mail.ru>
2024-11-06 12:40:32 +0300

    fix error 5

--- src/app/[locale]/(pages)/test/page.tsx
+++ src/app/[locale]/(pages)/test/page.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import {PriceCardFeach} from "@/app/[locale]/components/features";
 
 const Page = () => {
     return (
         <div>
-            <PriceCardFeach/>
+
         </div>
     );
 };
