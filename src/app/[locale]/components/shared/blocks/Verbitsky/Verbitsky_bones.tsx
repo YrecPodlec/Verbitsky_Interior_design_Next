2a619298d62dab50f8a0bdc0465581b9de780b07
YrecPodlecc <000333555666@mail.ru>
2024-11-11 22:45:04 +0300

    photo

--- src/app/[locale]/components/shared/blocks/Verbitsky/Verbitsky_bones.tsx
+++ src/app/[locale]/components/shared/blocks/Verbitsky/Verbitsky_bones.tsx
@@ -8,7 +8,7 @@ const VerbitskyBones = () => {
         <div className={'w-[45%] flex justify-end items-center z-20 xs:hidden'}>
             <div className={'text-center'}>
                 <div className={'relative w-[20vw] h-[50vh] s:w-[230] s:h-[356]'}>
-                    <Image src={'/verbitsky irina.webp'} alt={'Irina Verbitsky The Designer Photo'} fill quality={100}
+                    <Image src={'/photo.jpg'} alt={'Irina Verbitsky The Designer Photo'} fill quality={100}
                            style={{objectFit: "cover"}} className={'rounded-main'}/>
                 </div>
                 <div className={'responsive-32'}>
